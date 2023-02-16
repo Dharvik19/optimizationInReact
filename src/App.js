9b62454b013e574d10948239577ee530165fdfcc
Dharvik19 <dharvik.badga22@gmail.com>
2023-02-16 21:26:28 +0530

    optimization done with ascending,descending button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React, { useState, useCallback, useMemo } from 'react';
-
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import DemoList from './components/Demo/DemoList';
 import Button from './components/UI/Button/Button';
 
 function App() {
+  console.log("App.js running");
   const [listTitle, setListTitle] = useState('My List');
+  const [buttonName, setButtonName] = useState('Change to descending');
+  const [list, setList] = useState([5, 3, 1, 10, 9]);
+  const [ascend, setAscend] = useState(true);
 
   const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
+    setListTitle('New List');
   }, []);
 
-  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
-
+  const changeOrderHandler = useCallback(() =>{
+    if(ascend){
+        setListTitle('Ascending');
+        setButtonName('Change to Descending');
+        setList(list.sort((a, b)=>a-b));
+        setAscend(false);
+    }else{
+        setListTitle('Descending');
+        setButtonName('Change to Ascending');
+        setList(list.sort((a, b)=> b-a));
+        setAscend(true);
+    }
+  },[ascend, list]);
   return (
     <div className="app">
-      <DemoList title={listTitle} items={listItems} />
+      <DemoList title={listTitle} items={list} />
       <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <Button data='sort button' onClick={changeOrderHandler}>{buttonName}</Button>
     </div>
   );
 }
